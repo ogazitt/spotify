498c1a04d916df12cbeae5aafa011cecf3a3ac3e
Omri Gazitt <ogazitt@gmail.com>
2017-05-29 18:14:59 -0700

    removed dependency on spotify API wrapper and hardened code

--- spotify.js
+++ spotify.js
@@ -1,28 +1,38 @@
-const SpotifyWebApi = require('spotify-web-api-node');
 const config = require('./config.json');
-
-// credentials are optional
-const spotifyApi = new SpotifyWebApi({
-  clientId: config.SPOTIFY_CLIENT_ID,
-  clientSecret: config.SPOTIFY_ACCESS_TOKEN
-});
 
 var accessToken = null;
 accessToken = config.SPOTIFY_ACCESS_TOKEN;
 
 const rp = require('request-promise');
+var retries = 1;
 
 function authorize() {
-  return spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    console.log('The access token expires in ' + data.body['expires_in']);
-    console.log('The access token is ' + data.body['access_token']);
+  return Promise.resolve()
+  .then(() => {
+    var base64creds = new Buffer(`${config.SPOTIFY_CLIENT_ID}:${config.SPOTIFY_CLIENT_SECRET}`).toString('base64');
+    var options = {
+        method: 'POST',
+        uri: `https://accounts.spotify.com/api/token`,
+        headers: {
+          'User-Agent': 'Request-Promise',
+          'Authorization': `Basic ${base64creds}`
+        },
+        form: {
+          'grant_type': 'client_credentials'
+        },
+        json: true // Automatically parses the JSON string in the response 
+    };
+  
+    return rp(options);
+  })
+  .then(function (data) {
+    console.log('The access token expires in ' + data.expires_in);
+    console.log('The access token is ' + data.access_token);
 
     // Save the access token so that it's used in future calls
-    accessToken = data.body['access_token'];
-    spotifyApi.setAccessToken(accessToken);
+    accessToken = data.access_token;
   }, function(err) {
-        console.log('Something went wrong when retrieving an access token', err);
+     console.log('Something went wrong when retrieving an access token', err);
   });
 }
 
@@ -52,34 +62,17 @@ exports.getSongInfo = function getSongInfo(songInfo) {
       return url;
   })
   .catch(function (err) {
-      console.log('Error detected');
+      console.log(`Error detected: ${err}`);
+      // if the error is unauthorized access then try again
+      if (err.statusCode == 401 && retries > 0) {
+        // try again with a fresh access token
+        console.log('Retrying with a fresh auth token');
+        accessToken = null;
+        retries--;
+        return getSongInfo(songInfo);
+      }
+      console.log('No more retries');
       return '';
       // API call failed... 
   });
 }
-
-/*
-function spotifySearch (songInfo) {
-  let query = `/v1/search?query=${songInfo.band}%20${songInfo.song}&type=artist,track`;
-  var options = {
-    host: 'api.spotify.com',
-    port: 443,
-    path: query,
-    method: 'GET'
-  };
-
-  var req = https.request(options, function(res) {
-    console.log(res.statusCode);
-    res.on('data', function(d) {
-      process.stdout.write(d);
-      var json = JSON.parse(d);
-      track = json.tracks.items[0].href;
-      console.log(track);
-    });
-  });
-  req.end();
-  req.on('error', function(e) {
-    console.error(e);
-  });
-}
-*/
